162da64278d7d4d9a46a822b2feb370dbdf6f29f
paromitaprogga10-lgtm <paromitaprogga10@gmail.com>
2026-05-18 02:37:35 +0600

    progga

--- View/auctionResult.php
+++ View/auctionResult.php
@@ -1,5 +1,20 @@
 <?php
-echo "<h2>Auction Result</h2>";
+include "../Model/db.php";
+include "../Model/ResultModel.php";
+session_start();
 
+$isLoggedIn = $_SESSION["isLoggedIn"] ?? false;
+$name = $_SESSION["name"] ?? "";
+$role = $_SESSION["role"] ?? "";
+
+if (!$isLoggedIn) {
+    Header("Location: login.php");
+    exit();
+}
+
+if ($role != "admin") {
+    Header("Location: auctionList.php");
+    exit();
+}
 
 ?>
