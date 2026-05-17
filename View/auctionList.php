dcf4d05dd71b22c615aa840a051e64de5478dd50
marup00800 <marup.hossain00900@gmail.com>
2026-05-17 21:41:16 +0600

    2

--- View/auctionList.php
+++ View/auctionList.php
@@ -1,7 +1,19 @@
-<?php
+<html>
 
-echo "Session started";
+<head>
+    <title>Auction List</title>
+</head>
 
+<body>
 
+    <h3>Active Auctions</h3>
 
-?>
+    <label>Filter by Category:</label>
+
+    <select>
+        <option>All Categories</option>
+    </select>
+
+    <input type="text" placeholder="Search auctions...">
+
+    <p></p>
