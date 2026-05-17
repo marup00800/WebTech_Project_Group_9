eb7813168fa328a83d1856b574fc69dd7da64783
itsmesaima06 <Saimaakterrichi06@gmail.com>
2026-05-18 02:35:28 +0600

    bc

--- Controller/BidController.php
+++ Controller/BidController.php
@@ -1,4 +1,25 @@
 <?php
-echo "BidController.php";
+include "../Model/db.php";
+include "../Model/BidModel.php";
+include "../Model/ResultModel.php";
+session_start();
+
+header("Content-Type: application/json");
+
+$isLoggedIn = $_SESSION["isLoggedIn"] ?? false;
+
+if (!$isLoggedIn) {
+    echo json_encode(["ok" => false, "error" => "You must be logged in to place a bid."]);
+    exit();
+}
+
+$listing_id = $_POST["listing_id"] ?? "";
+$amount = $_POST["amount"] ?? "";
+$buyer_id = $_SESSION["user_id"];
+
+if (!$listing_id || !$amount) {
+    echo json_encode(["ok" => false, "error" => "Listing ID and amount are required."]);
+    exit();
+}
 
 ?>
