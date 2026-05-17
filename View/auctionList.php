8e1ddec5368412634f08a1a6870bf244a69d4418
marup00800 <marup.hossain00900@gmail.com>
2026-05-17 21:42:19 +0600

    t

--- View/auctionList.php
+++ View/auctionList.php
@@ -6,6 +6,8 @@
 
 <body>
 
+    <h2>Hello, User</h2>
+
     <h3>Active Auctions</h3>
 
     <label>Filter by Category:</label>
@@ -17,3 +19,24 @@
     <input type="text" placeholder="Search auctions...">
 
     <p></p>
+
+    <div>
+
+        <table border="1">
+
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Current Bid</th>
+                <th>Bid Count</th>
+                <th>Time Remaining</th>
+                <th>Action</th>
+            </tr>
+
+        </table>
+
+    </div>
+
+</body>
+
+</html>
