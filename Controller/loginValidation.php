38611257d944b6e8e1660b6fc28c0f05fbfbef2e
marup00800 <marup.hossain00900@gmail.com>
2026-05-17 17:07:22 +0600

    1

--- Controller/loginValidation.php
+++ Controller/loginValidation.php
@@ -1,11 +1,10 @@
 <?php
+include "../Model/db.php";
+include "../Model/UsersModel.php";
 session_start();
 
 $email = $_POST["email"] ?? "";
 $password = $_POST["password"] ?? "";
-
-$hasEmailError = false;
-$hasPasswordError = false;
 
 if (!$email) {
     $_SESSION["emailErr"] = "Email is required";
@@ -26,5 +25,34 @@ if (!$password) {
 if ($hasEmailError || $hasPasswordError) {
     $_SESSION["email"] = $email;
     Header("Location: ../View/login.php");
-    exit();
+} else {
+    $db = new db();
+    $connection = $db->openConnection();
+    $usersModel = new UsersModel();
+
+    $result = $usersModel->getUserByEmail($connection, "users", $email); 
+    if ($result->num_rows == 1) {
+        while ($row = $result->fetch_assoc()) {
+            if (password_verify($password, $row["password_hash"])) {
+                $_SESSION["user_id"] = $row["id"];
+                $_SESSION["name"] = $row["name"];
+                $_SESSION["role"] = $row["role"];
+                $_SESSION["seller_verified"] = $row["seller_verified"];
+                $_SESSION["isLoggedIn"] = true;
+
+                if ($row["role"] == "admin") {
+                    Header("Location: ../View/dashboard.php");
+                } else {
+                    Header("Location: ../View/auctionList.php");
+                }
+            } else {
+                $_SESSION["loginErr"] = "Email or password doesn't match. Please try again.";
+                Header("Location: ../View/login.php");
+            }
+        }
+    } else {
+        $_SESSION["loginErr"] = "Email or password doesn't match. Please try again.";
+        Header("Location: ../View/login.php");
+    }
 }
+?>
